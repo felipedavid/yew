5118d893aa1e7c9a2afcd889a5adfa60d3703bf7
Felipe David <felipedavid.huh@gmail.com>
2021-09-14 10:20:54 -0300

    starting over again

--- src/cpp/build.bat
+++
@@ -1,4 +0,0 @@
-if not exist ..\..\bin\ {
-    mkdir ..\..\bin\
-}
-cl.exe /Zi /EHsc main.cpp scanner.cpp token.cpp -o ..\..\bin\tsundere.exe
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+	std::cout << "Hello, Sailor" << std::endl;
+	return 0;
+}
