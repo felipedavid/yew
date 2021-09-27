d5848a4a1961055f4702198773de4bc89dbc2eb9
Felipe David <felipedavid.huh@gmail.com>
2021-09-27 10:22:26 -0300

    scanning keywords

--- src/fei/Scanner.java
+++ src/fei/Scanner.java
@@ -13,6 +13,27 @@ class Scanner {
 	private int start = 0;
 	private int current = 0;
 	private int line = 1;
+	private static final Map<String, TokenType> keywords;
+	
+	static {
+		keywords = new HashMap<>();
+		keywords.put("and",    AND);
+		keywords.put("class",  CLASS);
+		keywords.put("else",   ELSE);
+		keywords.put("false",  FALSE);
+		keywords.put("for",    FOR);
+		keywords.put("fun",    FUN);
+		keywords.put("if",     IF);
+		keywords.put("nil",    NIL);
+		keywords.put("or",     OR);
+		keywords.put("print",  PRINT);
+		keywords.put("return", RETURN);
+		keywords.put("super",  SUPER);
+		keywords.put("this",   THIS);
+		keywords.put("true",   TRUE);
+		keywords.put("var",    VAR);
+		keywords.put("while",  WHILE);
+	}
 	
 	Scanner(String source) {
 		this.source = source; 
@@ -58,12 +79,22 @@ class Scanner {
 			default:
 				if (isDigit(c)) {
 					number();
+				} else if (isAlpha(c)){
+					identifier();
 				} else {
 					Fei.error(line, "Unexpected character.");
 				}
-				Fei.error(line, "Unexpected character.");
 				break;
 		}
+	}
+	
+	private void identifier() {
+		while (isAlphaNumeric(peek())) advance();
+		
+		String text = source.substring(start, current);
+		TokenType type = keywords.get(text);
+		if (type == null) type = IDENTIFIER;
+		addToken(type);
 	}
 	
 	private void number() {
@@ -108,12 +139,23 @@ class Scanner {
 	}
 	
 	private char peek() {
+		if (isAtEnd()) return '\0';
 		return source.charAt(current);
 	}
 	
 	private char peekNext() {
 		if (current + 1 >= source.length()) return '\0';
 		return source.charAt(current+1);
+	}
+	
+	private boolean isAlpha(char c) {
+		return (c >= 'a' && c <= 'z') ||
+			   (c >= 'A' && c <= 'Z') ||
+			    c == '_';
+	}
+	
+	private boolean isAlphaNumeric(char c) {
+		return isAlpha(c) || isDigit(c);
 	}
 	
 	private boolean isDigit(char c) {
