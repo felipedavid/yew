bf26c438b9bc99adc30f09fb1005cf244ed9d7d6
Felipe David <felipedavid.huh@gmail.com>
2021-11-13 16:39:26 -0300

    Bug fix and adding a test to stretchy buffer

--- src/stretchy_buffer.c
+++ src/stretchy_buffer.c
@@ -19,3 +19,20 @@ void *buf__grow(void *buf, size_t new_len, size_t elem_size) {
 
     return new_buf->buf;
 }
+
+void buf_test() {
+    int *buf = NULL;
+    enum { N = 1024 };
+    for (int i = 0; i < N; i++) {
+        buf_push(buf, i);
+    }
+
+    assert(buf_len(buf) == N);
+    for (int i = 0; i < N; i++) {
+        assert(buf[i] == i);
+    }
+    buf_free(buf);
+    
+    assert(buf_cap(buf) == 0);
+    assert(buf_len(buf) == 0);
+}
