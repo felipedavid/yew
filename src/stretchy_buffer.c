ca8e9ca764a4f7ecd95a461a40f2a90b8afb94c2
Felipe David <felipedavid.huh@gmail.com>
2021-11-13 05:22:05 -0300

    Implementing dynamic arrays

---
+++ src/stretchy_buffer.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <assert.h>
+
+#include "stretchy_buffer.h"
+
+void *buf__grow(void *buf, size_t new_len, size_t elem_size) {
+    size_t new_cap = MAX(buf_cap(buf) * 2 + 1, new_len);
+    assert(new_len <= new_cap);
+    size_t new_size = offsetof(BufHdr, buf) + (new_cap * elem_size);
+
+    BufHdr *new_buf;
+    if (buf) {
+        new_buf = realloc(buf__hdr(buf), new_cap);
+    } else {
+        new_buf = malloc(new_cap);
+        new_buf->len = 0;
+    }
+    new_buf->cap = new_len;
+
+    return new_buf->buf;
+}
--- src/yew.c
+++ src/yew.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "stretchy_buffer.h"
+
 int main(int argc, char **argv) {
     if (argc < 2) {
         printf("Usage: ./yew program.yew\n");
